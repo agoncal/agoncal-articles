628a0591d12c82f28725b7b20c4dc6465d171446
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-07-07 09:40:46 +0200

    updating code

--- 2021-06-NubesGen-Terraform/src/main/java/org/agoncal/article/nubesgen/terraform/GreetingResource.java
+++ 2021-06-NubesGen-Terraform/src/main/java/org/agoncal/article/nubesgen/terraform/GreetingResource.java
@@ -4,13 +4,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.time.Instant;
 
+// tag::adocSnippet[]
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello NubesGen " + Instant.now();
     }
 }
+// end::adocSnippet[]
