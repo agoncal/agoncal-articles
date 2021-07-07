b0ae0130d514d9b4143444bf7807d898aa75b06e
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-07-07 09:30:12 +0200

    Adoc

--- 2021-06-NubesGen-Terraform/bootstrap.sh
+++ 2021-06-NubesGen-Terraform/bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+# tag::adocSnippet[]
 mvn -U io.quarkus:quarkus-maven-plugin:create \
         -DprojectGroupId=org.agoncal.article \
         -DprojectArtifactId=quarkus-nubesgen-terraform \
         -DpackageName="org.agoncal.article.nubesgen.terraform" \
         -Dextensions="quarkus-resteasy-jsonb"
+# end::adocSnippet[]
